e652fa5873b2bcbe729c4a503c990a8a54a8fd40
numalis804 <emanuele.lisetti@gmail.com>
2025-10-16 15:17:23 +0200

    Migrated tf state from local to remote backend

--- 03-basics/aws-backend/main.tf
+++ 03-basics/aws-backend/main.tf
@@ -4,13 +4,13 @@ terraform {
   ## YOU WILL UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
   ## TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
   #############################################################
-  # backend "s3" {
-  #   bucket         = "devops-directive-tf-state" # REPLACE WITH YOUR BUCKET NAME
-  #   key            = "03-basics/import-bootstrap/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "terraform-state-locking"
-  #   encrypt        = true
-  # }
+   backend "s3" {
+     bucket         = "terraform-practice-tf-state-804" # REPLACE WITH YOUR BUCKET NAME
+     key            = "03-basics/import-bootstrap/terraform.tfstate"
+     region         = "us-east-1"
+     dynamodb_table = "terraform-state-locking"
+     encrypt        = true
+   }
 
   required_providers {
     aws = {
@@ -22,10 +22,11 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  profile = "terraform_practice_user"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "devops-directive-tf-state" # REPLACE WITH YOUR BUCKET NAME
+  bucket        = "terraform-practice-tf-state-804" # REPLACE WITH YOUR BUCKET NAME
   force_destroy = true
 }
 
